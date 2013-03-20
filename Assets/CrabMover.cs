7b3805be737add17e88f1e662d6d3071f6edfe04
Andrew <awh32@case.edu>
2013-03-20 13:06:02 -0400

    Crab script, probably works, I'll test when I can before Thursday

---
+++ Assets/CrabMover.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Collections;
+
+public class CrabMover : MonoBehaviour {
+ 
+public float CrabMoveSpeed;
+private Vector3 TargetPosition;
+private Transform CrabTransform;
+ 
+ 
+void Start () 
+{
+    CrabTransform = transform;
+	TargetPosition = CrabTransform.position;
+}
+ 
+void Update () 
+{          
+    //Amount to move, move speed
+    float amtToMove = CrabMoveSpeed * Time.deltaTime;
+ 
+    //Move the Player after they have clicked the Left Mouse Button on a location
+    if (Input.GetMouseButtonDown(0))
+    {
+       Plane playerPlane = new Plane(Vector3.up, CrabTransform.position);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float hitdist = 0.0f;
+ 
+       if (playerPlane.Raycast(ray, out hitdist)) 
+       {
+            Vector3 targetPoint = ray.GetPoint(hitdist);
+            TargetPosition = ray.GetPoint(hitdist);
+            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+            CrabTransform.rotation = targetRotation;
+       }               
+    }
+ 
+    CrabTransform.position = Vector3.Lerp(CrabTransform.position, TargetPosition, amtToMove);
+}
+}
