7c8bf7221bdda227480c032b49d889d177bb43d6
Andrew <awh32@case.edu>
2013-03-20 21:46:13 -0400

    Tested CrabMover.cs

--- Assets/CrabMover.cs
+++ Assets/CrabMover.cs
@@ -1,3 +1,7 @@
+/*Andrew Heckman
+ * EECS 290
+ * Strategy game
+ */
 using UnityEngine;
 using System.Collections;
 
@@ -20,6 +24,8 @@ void Update ()
     float amtToMove = CrabMoveSpeed * Time.deltaTime;
  
     //Move the Player after they have clicked the Left Mouse Button on a location
+	//TODO: stop it from moving in the 3rd dimension (Might just require camera manipulation!)
+	//TODO: slow down movement
     if (Input.GetMouseButtonDown(0))
     {
        Plane playerPlane = new Plane(Vector3.up, CrabTransform.position);
