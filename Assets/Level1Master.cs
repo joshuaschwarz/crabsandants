c71f1c230b44b59dd15e4abcb925394453207d37
Robert Meyer <rsm78@case.edu>
2013-04-03 22:24:49 -0400

    Made a simple level.
    My script that is supposed to move to the next level isn't working. If
    you could check it i think its a simple fix.  Im just not sure if
    checking if there are no more crabs then if a score has been recorded.

--- Assets/Level1Master.cs
+++ Assets/Level1Master.cs
@@ -10,8 +10,8 @@ public class Level1Master : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Scored(){
-		if(GameObject.Find("Crab")) {
-			Application.LoadLevel("level 2");
+		if(!GameObject.Find("Crab")) {
+			Application.LoadLevel("Level 2");
 	}
 }
 }
